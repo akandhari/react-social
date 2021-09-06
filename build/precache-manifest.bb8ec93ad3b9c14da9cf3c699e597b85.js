e269caa9c1b7fa37336f494ce9f4e8a378ae7a83
Aakash Kandhari <aakash_kandhari@dell.com>
2021-09-06 17:03:18 -0500

    added ckin

---
+++ build/precache-manifest.bb8ec93ad3b9c14da9cf3c699e597b85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "33970d3f6c546da5a6c0",
+    "url": "/react-social/static/js/main.e947467c.chunk.js"
+  },
+  {
+    "revision": "4cc7e5170c03c71b96c2",
+    "url": "/react-social/static/js/runtime~main.f4f3335b.js"
+  },
+  {
+    "revision": "89fdb5f5af3629e47f47",
+    "url": "/react-social/static/css/2.d2d1c4ea.chunk.css"
+  },
+  {
+    "revision": "89fdb5f5af3629e47f47",
+    "url": "/react-social/static/js/2.1f2b26f6.chunk.js"
+  },
+  {
+    "revision": "5c18d03b894a1c8ef65363be314eb80b",
+    "url": "/react-social/static/media/avatar.5c18d03b.jpg"
+  },
+  {
+    "revision": "586806cd047e33b5ee7a43fc4252208d",
+    "url": "/react-social/static/media/mountains.586806cd.jpg"
+  },
+  {
+    "revision": "5cc6a04258f01cbd7961518cb91aa853",
+    "url": "/react-social/index.html"
+  }
+];
